73e398846be014d4d3ffb844d49d922dabce300c
abhisn15 <abhisuryanu9roho@gmail.com>
2024-07-13 11:24:59 +0700

    Created Almost Done~~

--- script.js
+++ script.js
@@ -27,4 +27,24 @@ const btnScrollToTop = document.querySelector(".btnScrollToTop");
     } else {
       btnScrollToTop.classList.remove("active");
     }
-})
+		})
+
+		document.addEventListener("DOMContentLoaded", () => {
+			const loadingScreen = document.getElementById("loading-screen");
+			const content = document.getElementById("content");
+			const page = document.querySelector("html");
+
+			// Add the class to prevent scrolling
+			page.classList.add("overflow-hidden");
+
+			// Simulate loading for demonstration purposes
+			setTimeout(() => {
+				loadingScreen.classList.add("hidden-opacity");
+				page.classList.remove("overflow-hidden");
+				setTimeout(() => {
+					loadingScreen.style.display = "none";
+					content.classList.remove("hidden");
+					content.classList.add("visible");
+				}, 1000); // 1000 milliseconds = 1 second to match the transition duration
+			}, 2000); // 2000 milliseconds = 2 seconds
+		});
