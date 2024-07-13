dc5c077a45edbfdfd35ac8829ef0526a8f3518e5
abhisn15 <abhisuryanu9roho@gmail.com>
2024-07-13 11:13:13 +0700

    Created Almost Done~~

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ const heroObserver = new IntersectionObserver(
 );
 
 heroObserver.observe(heroElement);
+
+// Animation Scroll
+const btnScrollToTop = document.querySelector(".btnScrollToTop");
+
+  btnScrollToTop.addEventListener("click", function(){
+    window.scrollTo(0, 0);
+  });
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      btnScrollToTop.classList.add("active");
+    } else {
+      btnScrollToTop.classList.remove("active");
+    }
+})
