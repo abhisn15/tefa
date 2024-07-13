1427a7ec7932048045a17b56d9ea77ba1d7a7190
abhisn15 <abhisuryanu9roho@gmail.com>
2024-07-13 11:28:09 +0700

    Created Almost Done~~

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Observer for header scroll effect
 const heroElement = document.querySelector("section.hero");
 const headerElement = document.querySelector("header");
 
 const heroObserver = new IntersectionObserver(
-	(entries) => {
+	function (entries) {
 		const [entry] = entries;
 		if (!entry.isIntersecting) {
 			headerElement.classList.add("scrolled");
@@ -18,33 +19,37 @@ heroObserver.observe(heroElement);
 // Animation Scroll
 const btnScrollToTop = document.querySelector(".btnScrollToTop");
 
-  btnScrollToTop.addEventListener("click", function(){
-    window.scrollTo(0, 0);
-  });
-  window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 100) {
-      btnScrollToTop.classList.add("active");
-    } else {
-      btnScrollToTop.classList.remove("active");
-    }
-		})
+btnScrollToTop.addEventListener("click", function () {
+	window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
-		document.addEventListener("DOMContentLoaded", () => {
-			const loadingScreen = document.getElementById("loading-screen");
-			const content = document.getElementById("content");
-			const page = document.querySelector("html");
+window.addEventListener("scroll", function () {
+	if (window.pageYOffset > 100) {
+		btnScrollToTop.classList.add("active");
+	} else {
+		btnScrollToTop.classList.remove("active");
+	}
+});
 
-			// Add the class to prevent scrolling
-			page.classList.add("overflow-hidden");
+// Loading screen
+document.addEventListener("DOMContentLoaded", function () {
+	const loadingScreen = document.getElementById("loading-screen");
+	const content = document.getElementById("content");
+	const page = document.querySelector("html");
 
-			// Simulate loading for demonstration purposes
-			setTimeout(() => {
-				loadingScreen.classList.add("hidden-opacity");
-				page.classList.remove("overflow-hidden");
-				setTimeout(() => {
-					loadingScreen.style.display = "none";
-					content.classList.remove("hidden");
-					content.classList.add("visible");
-				}, 1000); // 1000 milliseconds = 1 second to match the transition duration
-			}, 2000); // 2000 milliseconds = 2 seconds
-		});
+	// Add the class to prevent scrolling
+	page.classList.add("overflow-hidden");
+
+	// Simulate loading for demonstration purposes
+	setTimeout(function () {
+		loadingScreen.classList.add("hidden-opacity");
+		setTimeout(function () {
+			loadingScreen.style.display = "none";
+			content.classList.remove("hidden");
+			content.classList.add("visible");
+
+			// Remove the class to allow scrolling
+			page.classList.remove("overflow-hidden");
+		}, 1000); // 1000 milliseconds = 1 second to match the transition duration
+	}, 2000); // 2000 milliseconds = 2 seconds
+});
